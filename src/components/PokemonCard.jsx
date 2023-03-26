ed25e9eba09653385dbbfe28241bfab8778eb322
Vinch <vince.daviaud@gmail.com>
2023-03-26 01:32:49 +0100

    add bulbasaur card component

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,10 @@
+function PokemonCard() {
+  return (
+    <figure>
+      <img src="./src/assets/imgs/bulbasaur.png" alt="bulbasaur" />
+      <figcaption>Bulbasaur</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
